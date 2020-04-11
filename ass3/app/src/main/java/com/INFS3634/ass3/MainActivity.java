74622484397ad521b6c1dddb769295dd30cf8944
Mohamad Fozan <mfozan2@gmail.com>
2020-04-11 18:51:04 +1000

    Intial commit for assignment3

---
+++ ass3/app/src/main/java/com/INFS3634/ass3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.INFS3634.ass3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.chucknorris.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        k
+    }
+}
