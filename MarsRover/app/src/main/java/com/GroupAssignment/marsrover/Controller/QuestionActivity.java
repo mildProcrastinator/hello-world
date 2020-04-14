a5dbbb224342a82b71c1ec887dcf29613715d4ba
Mohamad Fozan <mfozan2@gmail.com>
2020-04-14 12:46:31 +1000

    background

---
+++ MarsRover/app/src/main/java/com/GroupAssignment/marsrover/Controller/QuestionActivity.java
@@ -0,0 +1,15 @@
+package com.GroupAssignment.marsrover.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.GroupAssignment.marsrover.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
--- MarsRover/app/src/main/java/com/GroupAssignment/marsrover/MainActivity.java
+++ MarsRover/app/src/main/java/com/GroupAssignment/marsrover/MainActivity.java
@@ -24,41 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Question currQuestion = questions.get(0);
-        for(int i=0;i<currQuestion.getKeys().length();i++){
-            addView(((LinearLayout) findViewById(R.id.layoutParent)), Character.toString(currQuestion.getKeys().charAt(i)), ((EditText) findViewById(R.id.editText)));
-        }
-
     }
 
-    private void addView(LinearLayout viewParents, final String text, final EditText editText) {
-        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
 
-        linearLayoutParams.rightMargin = 30;
-
-        final TextView textView = new TextView(this);
-
-        textView.setLayoutParams(linearLayoutParams);
-        textView.setBackground(this.getResources().getDrawable(R.drawable.bgpink));
-        textView.setTextColor(this.getResources().getColor(R.color.colorPurple));
-        textView.setGravity(Gravity.CENTER);
-        textView.setText(text);
-        textView.setClickable(true);
-        textView.setFocusable(true);
-        textView.setTextSize(32);
-
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
-
-        textQuestion = (TextView) findViewById(R.id.textQuestion);
-        textScreen = (TextView) findViewById(R.id.textScreen);
-        textTitle = (TextView) findViewById(R.id.textTitle);
-
-        textQuestion.setTypeface(typeface);
-        textScreen.setTypeface(typeface);
-        textTitle.setTypeface(typeface);
-
-    }
 }
